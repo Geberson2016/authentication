0cae95d5f67b2c534e208f6011fa44dcc747f6a8
Geberson Antonio <geberson2016@gmail.com>
2022-06-09 18:43:04 -0300

    Models add

---
+++ src/main/java/com/back/authentication/models/User.java
@@ -0,0 +1,74 @@
+package com.back.authentication.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username")
+        })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 28)
+    private String username;
+
+    @NotBlank
+    @Size(max = 120)
+    private String name;
+
+    @NotBlank
+    @Size(max = 60)
+    private String password;
+
+    //Falta as Roles
+
+    public User(){
+
+    }
+
+    public User(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
